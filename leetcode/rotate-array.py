f6d903235aa8c21c5919371658cc782110cbfb91
Juniel Djossou <42700556+MizuPanda@users.noreply.github.com>
2025-10-20 21:04:30 -0400

    Update solution for rotate-array

--- leetcode/rotate-array.py
+++ leetcode/rotate-array.py
@@ -1,21 +1,13 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> newNums = {};
-        const int s = nums.size();
-        
-        k = k % s;
-        if (k == 0) {
-            return ;
-        }
-        for (int i = 0; i < s; ++i) {
-            if (i < k) {
-                newNums.emplace_back(nums.at(s - k + i));
-            } else {
-                newNums.emplace_back(nums.at(i - k));
-            }
-        }
+        const vector<int> copy = nums;
+        const int len = nums.size();
 
-        nums = newNums;
+        for (size_t i = 0; i < len; ++i) {
+            const int pos = (i + k) % len;
+
+            nums.at(pos) = copy.at(i);
+        }
     }
 };
