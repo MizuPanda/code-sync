69ab0e6ccb46d426c2d75470efa7565b78018a32
Juniel Djossou <42700556+MizuPanda@users.noreply.github.com>
2025-12-27 22:53:40 -0500

    Update solution for valid-palindrome

--- leetcode/valid-palindrome.py
+++ leetcode/valid-palindrome.py
@@ -1,6 +1,44 @@
 class Solution {
 public:
+
     bool isPalindrome(string s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (end > start) {
+            const char startChar = s.at(start);
+            const char endChar = s.at(end);
+
+            if (!isAlphanumeric(startChar)) {
+                ++start;
+            } else if (!isAlphanumeric(endChar)) {
+                --end;
+            } else if (isSameChars(startChar, endChar)) {
+                ++start;
+                --end;
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    bool isAlphanumeric(char x) {
+        return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || (x >= '0' && x <= '9');
+    }
+
+    bool isSameChars(char x, char y) {
+
+        const char diff = 'a' - 'A';
+
+        if (x >= 'A' && x <= 'Z') {
+            x += diff;
+        }
+        if (y >= 'A' && y <= 'Z') {
+            y += diff;
+        }
         
+        return x == y;
     }
 };
