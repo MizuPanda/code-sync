ae4aed8a4aec553fa3e073842a56f83ce2b78f18
Juniel Djossou <42700556+MizuPanda@users.noreply.github.com>
2025-12-29 02:29:29 -0500

    Update solution for 3sum

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -9,17 +9,17 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size() - 2; ++i) {
-            const int target = -nums.at(i);
-
+            if (i > 0 && nums.at(i) == nums.at(i - 1)) continue;
+            
             int start = i + 1;
             int end = nums.size() - 1;
 
             while (end > start) {
-                const int sum = nums.at(start) + nums.at(end);
+                const int sum = nums.at(i) + nums.at(start) + nums.at(end);
 
-                if (sum > target) {
+                if (sum > 0) {
                     --end;
-                } else if (sum < target) {
+                } else if (sum < 0) {
                     ++start;
                 } else {
                     const string key = to_string(nums.at(i)) + to_string(nums.at(start)) + to_string(nums.at(end));
