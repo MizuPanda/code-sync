83cf28beb144efbc4be1ed634d7510d8bdfd5196
Juniel Djossou <42700556+MizuPanda@users.noreply.github.com>
2026-02-08 03:50:38 +0900

    Update solution for merge-two-sorted-lists

--- leetcode/merge-two-sorted-lists.py
+++ leetcode/merge-two-sorted-lists.py
@@ -11,12 +11,10 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if (list1 == nullptr && list2 == nullptr) {
-            return nullptr;
+        if (list1 == nullptr) {
+            return list2;
         } else if (list2 == nullptr) {
-            return new ListNode(list1->val, mergeTwoLists(list1->next, list2));
-        } else if (list1 == nullptr) {
-            return new ListNode(list2->val, mergeTwoLists(list1, list2->next));
+            return list1;
         } else if (list1->val < list2->val) {
             return new ListNode(list1->val, mergeTwoLists(list1->next, list2));
         } else {
