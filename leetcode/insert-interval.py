9e03880fd8c0f3ccf089cdef7327e91c141fdfa6
Juniel Djossou <junieldjossou@gmail.com>
2026-08-26 13:38:57 -0400

    Add solution for insert-interval

---
+++ leetcode/insert-interval.py
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        res.push_back(intervals.at(0));
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (res.back().at(1) >= intervals.at(i).at(0)) {
+                res.back().at(1) = max(res.back().at(1), intervals.at(i).at(1));
+            } else {
+                res.push_back(intervals.at(i));
+            }
+        }
+
+        return res;
+    }
+};
