b9fe34bd497c02d4673b173005328b2a3ff4d784
Juniel Djossou <junieldjossou@gmail.com>
2026-08-28 23:34:02 -0400

    Add solution for online-stock-span

---
+++ leetcode/online-stock-span.py
@@ -0,0 +1,29 @@
+class StockSpanner {
+private:
+    stack<pair<int, int>> s;
+
+public:
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+
+        int count = 1;
+        
+        while (!s.empty() && price >= s.top().first) {
+            count += s.top().second;
+            s.pop();
+        } 
+
+        s.emplace(price, count);
+
+        return count;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
